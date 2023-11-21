ef6a7259d082e6096ff050fcb225589f2452d502
Tonchavin Alexandr <bloodwayn@gmail.com>
2023-11-21 18:49:38 +1000

    add HW1

--- Homework1/SortList.py
+++ Homework1/SortList.py
@@ -1,2 +1,13 @@
+# Императивный стиль
+def sort_list_imperative(numbers):
+    pass
+    return numbers
+
+
+# Декларативный стиль
+def sort_list_declarative(numbers):
+    pass
+
+
 if __name__ == '__main__':
     print()
